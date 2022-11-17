ed0f19f5b75ec4f215c37b80a69fba99959f9c35
kowshi <k.thevathas1611@gmail.com>
2022-11-17 21:27:43 +0530

    database setup and create new user api

--- backend/game-app/src/main/java/com/pdsa/gameapp/model/User.java
+++ backend/game-app/src/main/java/com/pdsa/gameapp/model/User.java
@@ -1,27 +1,26 @@
-package model;
+package com.pdsa.gameapp.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	@GeneratedValue()
+	private Long id;
 	private String name;
 	
 	public User() {
 		
 	}
 
-	public int getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- backend/game-app/src/main/java/com/pdsa/gameapp/service/UserService.java
+++ backend/game-app/src/main/java/com/pdsa/gameapp/service/UserService.java
@@ -1,4 +1,4 @@
-package service;
+package com.pdsa.gameapp.service;
 
 public interface UserService {
 
