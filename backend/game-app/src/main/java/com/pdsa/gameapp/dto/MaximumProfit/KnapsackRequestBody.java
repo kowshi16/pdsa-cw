e7752621fc76b5ebad74bdcac10bb41cbcc0db42
kowshi <k.thevathas1611@gmail.com>
2022-11-19 13:58:57 +0530

    Maximum Profit game done

---
+++ backend/game-app/src/main/java/com/pdsa/gameapp/dto/MaximumProfit/KnapsackRequestBody.java
@@ -0,0 +1,35 @@
+package com.pdsa.gameapp.dto.MaximumProfit;
+
+import lombok.Data;
+
+@Data
+public class KnapsackRequestBody {
+	int weightItems[];
+	int profitItems[];
+	int userTotalProfit;
+	
+	public int[] getWeightItems() {
+		return weightItems;
+	}
+	
+	public void setWeightItems(int[] weightItems) {
+		this.weightItems = weightItems;
+	}
+	
+	public int[] getProfitItems() {
+		return profitItems;
+	}
+	
+	public void setProfitItems(int[] profitItems) {
+		this.profitItems = profitItems;
+	}
+	
+	public int getUserTotalProfit() {
+		return userTotalProfit;
+	}
+	
+	public void setUserTotalProfit(int userTotalProfit) {
+		this.userTotalProfit = userTotalProfit;
+	}
+
+}
